d43fee86edca7ef5830fe9cb7290aa187650716e
Shruti6197 <spprabhune@gmail.com>
2025-01-03 07:38:04 +0530

    Update TestNG_BaseClass.java

--- Advance_Automation/src/test/java/GenericUtility/TestNG_BaseClass.java
+++ Advance_Automation/src/test/java/GenericUtility/TestNG_BaseClass.java
@@ -18,7 +18,7 @@ import org.testng.annotations.Parameters;
 
 import pomRepository.LoginPage_Vtiger;
 import pomRepository.Vtiger_HomePage;
-//Pulling the code
+
 //pushing back to github
 public class TestNG_BaseClass {
 	public WebDriver driver;
